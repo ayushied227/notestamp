219873dd0b2a196fda9350b60e6250db749be321
Ayushi <ayushidufare22@gmail.com>
2022-12-25 00:57:19 +0530

    adding frontend

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from "./components/Header";
+import NotePage from "./pages/NotePage";
+import NotesListPage from "./pages/NotesListPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+     <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Route path="/" exact element={NotesListPage} />
+          <Route path="/note/:id" element={NotePage} />
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
